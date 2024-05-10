b2596cd4665e976eb99e2bf24aa530447df2d4b7
Ergen35 <mmhopeme@gmail.com>
2024-05-10 21:31:46 +0100

    Add project files.

---
+++ AppConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace BooksTracker.Models;
+
+
+public static class AppConfig
+{
+    public const string SUPABASE_URL = "http://auth-supa.synchro-us.com"; // Your Supabase URL
+    public const string SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzEzODI2ODAwLAogICJleHAiOiAxODcxNTkzMjAwCn0.mzDR1eK-dou7ql5kKMphFQnsfpWj0vl5IRZkkg7vL_A"; // Your Supabase Key
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BooksTracker.Models;
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+var supabaseClient = new Supabase.Client(BooksTracker.Models.AppConfig.SUPABASE_URL, BooksTracker.Models.AppConfig.SUPABASE_KEY);
+
+var books = await supabaseClient.From<Book>().Get();
+
+Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(books.Models));
+
+Console.ReadLine();
